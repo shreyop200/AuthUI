798b2bee5305f0bca37bab3b75906aa98347621f
Shreyansh <shreyanshop20+2@outlook.com>
2023-06-21 09:10:18 +0530

    PluginOwned Added

--- src/shreyop200/AuthUI/ReloadCommand.php
+++ src/shreyop200/AuthUI/ReloadCommand.php
@@ -5,19 +5,19 @@ declare(strict_types=1);
 namespace shreyop200\AuthUI;
 
 use pocketmine\command\Command;
-use pocketmine\command\CommandExecutor;
 use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginOwned;
 
-class ReloadCommand extends Command {
+class ReloadCommand extends Command implements PluginOwned {
 
     private $plugin;
 
-    public function __construct(Main $plugin) {
-        parent::__construct("loginreload", "Reload player data", "/loginreload", ["lr"]);
+    public function __construct(Main $plugin, string $name) {
+        parent::__construct($name, "Reload player data", "/$name", [$name]);
         $this->plugin = $plugin;
     }
 
-    public function execute(CommandSender $sender, string $commandLabel, array $args) {
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
         if ($sender->hasPermission("authui.command.reload")) {
             $this->plugin->reloadPlayerData();
             $sender->sendMessage("Player data reloaded.");
@@ -25,5 +25,9 @@ class ReloadCommand extends Command {
             $sender->sendMessage("You don't have permission to use this command.");
         }
         return true;
+    }
+
+    public function getOwningPlugin(): Main {
+        return $this->plugin;
     }
 }
