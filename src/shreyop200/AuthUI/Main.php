dee57b56ecfec46f873debb488e5751b0f77ae04
Shreyansh <shreyanshop20+2@outlook.com>
2023-08-28 16:10:21 +0530

    Fixed Issue #1 & #2

--- src/shreyop200/AuthUI/Main.php
+++ src/shreyop200/AuthUI/Main.php
@@ -27,17 +27,27 @@ class Main extends PluginBase implements Listener {
         $this->saveDefaultConfig();
         $this->reloadConfig();
         $this->config = $this->getConfig();
-
         $this->createDataFolder();
-
-        $this->database = new SQLite3($this->getDataFolder() . "player_data.db");
+        $this->database = new SQLite3($this->getDataFolder() . "auth.db", SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
         $this->initializeDatabase();
-
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getServer()->getCommandMap()->register($this->getName(), new ReloadCommand($this, $this->getName()));
+
+        $this->loadAllPlayerData();
+        $this->getLogger()->info("AuthUI Plugin has been enabled.");
+    }
+
+    public function onDisable(): void {
+        foreach ($this->getServer()->getOnlinePlayers() as $player) {
+            $playerName = $player->getName();
+            $playerData = $this->getPlayerData($player);
         
-        // Load player data when the server starts
-        $this->loadPlayerData();
+            if ($playerData !== null) {
+                $this->savePlayerData($player, $playerData);
+            }
+        }
+        $this->database->close();
+        $this->getLogger()->info("AuthUI Plugin has been disabled.");
     }
 
     private function createDataFolder(): void {
@@ -47,59 +57,176 @@ class Main extends PluginBase implements Listener {
         }
     }
 
+    public function reloadPlayerData(): void {
+        $this->players = [];
+        $this->loadAllPlayerData();
+        $this->getLogger()->info("Player data reloaded.");
+    }
+
     private function initializeDatabase(): void {
         $createTableQuery = "CREATE TABLE IF NOT EXISTS players (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             playerName TEXT NOT NULL,
             password TEXT NOT NULL,
-            loginStreak INTEGER DEFAULT 0
+            loginStreak INTEGER DEFAULT 0,
+            registered INTEGER DEFAULT 0
         )";
 
-        $this->database->exec($createTableQuery);
-    }
+        $result = $this->database->exec($createTableQuery);
 
-    private function loadPlayerData(): void {
-        $result = $this->database->query("SELECT * FROM players");
-        while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
-            $playerName = $row['playerName'];
-            $this->players[$playerName] = $row;
+        if ($result !== false) {
+            $this->getLogger()->info("Database table 'players' initialized.");
+        } else {
+            $this->getLogger()->error("Failed to initialize database table 'players'.");
         }
-        $result->finalize();
     }
 
-    private function hasRegistered(Player $player): bool {
-        $playerName = $player->getName();
-        return isset($this->players[$playerName]);
+    private function loadAllPlayerData(): void {
+        $query = "SELECT * FROM players";
+        $stmt = $this->database->prepare($query);
+
+        if ($stmt === false) {
+            $this->getLogger()->error("Failed to prepare database query: " . $this->database->lastErrorMsg());
+            return;
+        }
+
+        $result = $stmt->execute();
+
+        if ($result !== false) {
+            $loadedData = 0;
+
+            while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+                $playerName = $row['playerName'];
+                $this->players[$playerName] = $row;
+                $loadedData++;
+            }
+
+            $stmt->close();
+
+            if ($loadedData > 0) {
+                $this->getLogger()->info("Loaded data for $loadedData players.");
+            } else {
+                $this->getLogger()->warning("No player data found in the database.");
+            }
+        } else {
+            $this->getLogger()->error("Failed to execute database query: " . $this->database->lastErrorMsg());
+        }
     }
 
-    private function getPlayerData(Player $player): ?array {
+
+    private function loadPlayerData(Player $player): void {
         $playerName = $player->getName();
-        return $this->players[$playerName] ?? null;
+        $query = "SELECT * FROM players WHERE playerName = :name";
+        $stmt = $this->database->prepare($query);
+        $stmt->bindValue(":name", $playerName, SQLITE3_TEXT);
+        
+        $result = $stmt->execute();
+
+        if ($result !== false) {
+            $row = $result->fetchArray(SQLITE3_ASSOC);
+            if ($row !== false) {
+                $this->showLoginUI($player);
+            } else {
+                $this->showRegisterUI($player);
+            }
+            $stmt->close();
+        } else {
+            $this->getLogger()->error("Failed to execute database query: " . $this->database->lastErrorMsg());
+        }
+    }
+
+
+
+    /**
+     * @param Player $player
+     * @return bool|array Returns an array with player data if found, or false if not found.
+      */
+    private function getPlayerData(Player $player) {
+        $playerName = $player->getName();
+        $query = "SELECT * FROM players WHERE playerName = :name";
+        $stmt = $this->database->prepare($query);
+        $stmt->bindValue(":name", $playerName, SQLITE3_TEXT);
+        
+        $result = $stmt->execute();
+
+        if ($result !== false) {
+            $row = $result->fetchArray(SQLITE3_ASSOC);
+            $stmt->close();
+            return $row;
+        } else {
+            $this->getLogger()->error("Failed to execute database query: " . $this->database->lastErrorMsg());
+        }
+        
+        return null;
     }
 
     private function savePlayerData(Player $player, array $data): void {
         $playerName = $player->getName();
-        $this->players[$playerName] = $data;
-
-        $password = $this->database->escapeString($data['password']);
+        $password = $data['password'];
         $loginStreak = $data['loginStreak'];
+        $registered = isset($data['registered']) ? $data['registered'] : 0;
 
-        $existingData = $this->getPlayerData($player);
-        if ($existingData === null) {
-            $query = "INSERT INTO players (playerName, password, loginStreak) VALUES ('$playerName', '$password', $loginStreak)";
+        $query = "INSERT OR REPLACE INTO players (playerName, password, loginStreak, registered) VALUES (:name, :password, :loginStreak, :registered)";
+        $stmt = $this->database->prepare($query);
+        $stmt->bindValue(":name", $playerName, SQLITE3_TEXT);
+        $stmt->bindValue(":password", $password, SQLITE3_TEXT);
+        $stmt->bindValue(":loginStreak", $loginStreak, SQLITE3_INTEGER);
+        $stmt->bindValue(":registered", $registered, SQLITE3_INTEGER);
+
+        $result = $stmt->execute();
+
+        if ($result !== false) {
+            $stmt->close();
         } else {
-            $query = "UPDATE players SET password = '$password', loginStreak = $loginStreak WHERE playerName = '$playerName'";
+            $this->getLogger()->error("Failed to save data for player: $playerName");
+        }
+    }
+
+    private function hasRegistered(Player $player): bool {
+        $playerName = $player->getName();
+        $query = "SELECT registered FROM players WHERE playerName = :name";
+        $stmt = $this->database->prepare($query);
+        $stmt->bindValue(":name", $playerName, SQLITE3_TEXT);
+
+        $result = $stmt->execute();
+
+        if ($result !== false) {
+            $row = $result->fetchArray(SQLITE3_ASSOC);
+            $stmt->close();
+            return ($row !== false) && ($row['registered'] === 1);
+        } else {
+            $this->getLogger()->error("Failed to execute database query: " . $this->database->lastErrorMsg());
         }
 
-        $this->database->exec($query);
+        return false;
+    }
+
+    private function setPlayerRegistered(Player $player, bool $registered): void {
+        $playerName = $player->getName();
+        $registeredValue = $registered ? 1 : 0;
+        
+        $query = "UPDATE players SET registered = :registered WHERE playerName = :name";
+        $stmt = $this->database->prepare($query);
+        $stmt->bindValue(":registered", $registeredValue, SQLITE3_INTEGER);
+        $stmt->bindValue(":name", $playerName, SQLITE3_TEXT);
+        
+        $result = $stmt->execute();
+        
+        if ($result !== false) {
+            $stmt->close();
+        } else {
+            $this->getLogger()->error("Failed to set 'registered' value for player: $playerName");
+        }
     }
 
     public function onPlayerJoin(PlayerJoinEvent $event): void {
         $player = $event->getPlayer();
-        if (!$this->hasRegistered($player)) {
-            $this->showRegisterUI($player);
-        } else {
+        $playerName = $player->getName();
+
+        if ($this->hasRegistered($player)) {
             $this->showLoginUI($player);
+        } else {
+            $this->showRegisterUI($player);
         }
     }
 
@@ -118,6 +245,8 @@ class Main extends PluginBase implements Listener {
                     'loginStreak' => 0
                 ];
                 $this->savePlayerData($player, $playerData);
+                $this->setPlayerRegistered($player, true);
+
                 $player->sendMessage($this->config->get("register_success_message"));
             } else {
                 $player->kick($this->config->get("password_mismatch_kick_message"), false);
@@ -145,7 +274,11 @@ class Main extends PluginBase implements Listener {
             $password = $data[0];
 
             $playerData = $this->getPlayerData($player);
-            $savedPassword = $playerData['password'];
+            if ($playerData != null) {
+                $savedPassword = $playerData['password'];
+            } else {
+                return;
+            }
 
             if ($password === $savedPassword) {
                 $player->sendMessage($this->config->get("login_success_message"));
@@ -177,7 +310,6 @@ class Main extends PluginBase implements Listener {
         }
 
         $rewardsConfig = $this->config->get("login-reward", []);
-
         foreach ($rewardsConfig as $milestone => $command) {
             if ($loginStreak >= $milestone) {
                 $command = str_replace("{player}", $player->getName(), $command);
@@ -189,21 +321,4 @@ class Main extends PluginBase implements Listener {
         }
     }
 
-    public function unregisterPlayer(Player $player): void {
-        $playerName = $player->getName();
-        $this->database->exec("DELETE FROM players WHERE playerName = '$playerName'");
-        unset($this->players[$playerName]);
-        $player->sendMessage($this->config->get("unregister_success_message"));
-    }
-
-    public function reloadPlayerData(): void {
-        $this->players = [];
-        $this->loadPlayerData();
-        $this->getLogger()->info("Player data reloaded.");
-    }
-
-    public function onDisable(): void {
-        // Save player data when the server stops
-        $this->savePlayerData();
-    }
 }
